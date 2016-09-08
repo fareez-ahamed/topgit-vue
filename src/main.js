65f549f9877ff105cbd23a2e08c515c58ce84f94
Fareez Ahamed <fareez.ahamed@gmail.com>
2016-09-08 11:11:26 +0530

    Navbar and router added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
+import MainView from './components/MainView'
+import MyReposView from './components/MyReposView'
+
+Vue.use(VueRouter)
+
+let router = new VueRouter()
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
   components: { App }
+})
+
+router.map({
+  '/': {
+    component: MainView
+  },
+  '/myrepos': {
+    component: MyReposView
+  }
 })
