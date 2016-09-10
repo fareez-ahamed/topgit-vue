f870f9bf1acd22c35a75ec914db9df80bdf65a2f
Fareez Ahamed <fareez.ahamed@gmail.com>
2016-09-10 06:32:00 +0530

    Added basic component structure for MainView

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -1,11 +1,27 @@
 <template>
-This is Main View
+<div class="row">
+  <div class="col-md-8">
+    <form>
+      <div class="form-group">
+        <input type="text" ng-model="query" uib-typeahead="language for language in languages | filter:$viewValue | limitTo:8" ng-enter="search()" class="form-control" placeholder="Search by Language">
+      </div>
+    </form>
+    <div v-repeat="alert in alerts" class="alert alert-{{alert.type}}" role="alert">{{alert.msg}}</div>
+    <repositories></repositories>
+  </div>
+  <div class="col-md-4">
+    <filter-options></filter-options>
+    <rate-limit></rate-limit>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      alerts: []
+    }
   }
 }
 </script>
