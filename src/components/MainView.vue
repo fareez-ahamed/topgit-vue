351a5854135807524c2f60b7745000368e0f11ea
Fareez Ahamed <fareez.ahamed@gmail.com>
2016-09-11 18:34:28 +0530

    Pagination component implemented

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -3,11 +3,14 @@
   <div class="col-md-8">
     <form>
       <div class="form-group">
-        <input type="text" v-model="query" @keypress.enter="search" class="form-control" placeholder="Search by Language">
+        <input type="text" v-model="repoPagination.currentPage" @keypress.enter="search" class="form-control" placeholder="Search by Language">
       </div>
     </form>
     <div v-for="alert in alerts" class="alert alert-{{alert.type}}" role="alert">{{alert.msg}}</div>
     <repo v-for="repo in repos" :details="repo"></repo>
+    <pagination :current-page.sync="repoPagination.currentPage"
+                :total-items="repoPagination.totalItems"
+                :items-per-page="repoPagination.itemsPerPage"></pagination>
   </div>
   <div class="col-md-4">
     <filter-options></filter-options>
@@ -21,16 +24,21 @@ import github from '../services/Github'
 import Repo from './Repo'
 import RateLimit from './RateLimit'
 import FilterOptions from './FilterOptions'
+import Pagination from './Pagination'
 
 export default {
 
-  components: { Repo, RateLimit, FilterOptions },
+  components: { Repo, RateLimit, FilterOptions, Pagination },
 
   data () {
     return {
       query: '',
       minStar: 0,
-      currentPage: 1,
+      repoPagination: {
+        currentPage: 1,
+        totalItems: 151,
+        itemsPerPage: 5
+      },
       alerts: [],
       repos: []
     }
